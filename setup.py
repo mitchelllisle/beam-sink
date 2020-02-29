cc329f952ff73e37cd4b6b9191e7a13e8183ac2d
Mitchell Lisle <lislemitchell@gmail.com>
2020-02-29 11:44:49 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mitchelllisle/beam-sink',
-    version='0.7.0',
+    version='0.8.0',
     zip_safe=False,
 )
